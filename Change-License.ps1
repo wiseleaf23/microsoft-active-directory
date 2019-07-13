be34e96dce90fe06ece4f10965be7837aa31abfa
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2019-07-13 21:22:16 +0200

    Added files

---
+++ Change-License.ps1
@@ -0,0 +1,28 @@
+﻿#Run the command Get-MsolAccountSku to retrieve a list of the available SKU's
+
+#Variables
+    $oldSKU = "Spelenderwijs:STANDARDWOFFPACK" #old SKU, for example "Contoso:STANDARDWOFFPACK"
+    $newSKU = "Spelenderwijs:ATP_ENTERPRISE" #for instance "Contoso:STANDARDPACK"
+    $backupCSV = "C:\OfficeGrip\BackupCSV.csv" #location for backup CSV
+
+#Replace
+    #Wanna see the users and licenses first? Run $users | Select-Object DisplayName,Licenses
+    $users = Get-MsolUser -All | Where-Object {($_.licenses).AccountSkuId -match $oldSKU}
+    $users | Export-Csv -Path $backupCSV -NoTypeInformation
+    foreach ($user in $users) {
+        $upn=$user.userPrincipalName
+        Set-MsolUserLicense -UserPrincipalName $upn -AddLicenses $newSKU -RemoveLicenses $oldSKU
+    }
+
+#Add
+    Write-Host ""
+    Write-Host "Did you create a backup of all users' license assigment?" -ForegroundColor Yellow
+    Pause
+    $users = Get-MsolUser -All | Where-Object {$_.isLicensed -eq "TRUE"}
+    foreach ($user in $users) {
+        $upn=$user.userPrincipalName
+        Set-MsolUserLicense -UserPrincipalName $upn -AddLicenses $newSKU
+    }
+
+
+
---
+++ Change-M365Upn.ps1
@@ -0,0 +1,17 @@
+﻿#Changes UPN for Microsoft 365 accounts
+#For options with CSV: https://www.morgantechspace.com/2018/03/how-to-change-userprincipalname-of-office-365-user-powershell.html
+
+#Variables
+    $oldDomain = Read-Host -Prompt "Please enter the old suffix, for example: contoso.onmicrosoft.com"
+    $newDomain = Read-Host -Prompt "Please enter the new suffix, for example: contoso.com"
+    
+
+#Execute
+    #Get all users
+    Get-AzureADUser -All $True | Where { $_.UserPrincipalName.ToLower().EndsWith($oldDomain) }
+    #Loop trhrough users to change UPN
+    ForEach {
+         $newupn = $_.UserPrincipalName.Split("@")[0] + "@" + $newDomain
+         Write-Host "Changing UPN value from: "$_.UserPrincipalName" to: " $newupn -ForegroundColor Green
+         Set-AzureADUser -ObjectId $_.UserPrincipalName  -UserPrincipalName $newupn
+    }
