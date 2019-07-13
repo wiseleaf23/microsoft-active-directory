a601c30851980687842a4251aa78b3784081a3ad
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2019-07-13 21:03:31 +0200

    Add files via upload

---
+++ Get-M365AdminRoles.ps1
@@ -0,0 +1,28 @@
+﻿#Parameters
+    Param(
+        [Parameter(Mandatory=$false)]$ExportToCsvPath
+    )
+
+#Connect to MSOnline
+    Connect-MsolService
+
+#Get all roles
+    $Roles = Get-MsolRole
+    
+#Loop roles and get members
+    $RolesAndMembers = @()
+    ForEach($Role in $Roles) {
+        $RolesAndMembers += Get-MsolRoleMember -All -RoleObjectId $Role.ObjectId | Select-Object @{Name="Role";Expression={$Role.Name}},RoleMemberType,EmailAddress,DisplayName
+    }
+
+#Show on console or export to CSV
+    If($ExportToCsvPath) {
+        $RolesAndMembers | Export-Csv -Path $ExportToCsvPath -NoTypeInformation -Force
+    }
+    Else {
+        $RolesAndMembers
+    }
+
+#Pause, clear and exit
+    Read-Host -Prompt "Finished, press Enter to exit"
+    Exit
