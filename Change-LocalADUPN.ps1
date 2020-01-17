3666d05dd8f3412a5975a318f05b0e9f80bdc29f
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2020-01-17 09:42:58 +0100

    Updates for publication

---
+++ Change-LocalADUPN.ps1
@@ -0,0 +1,17 @@
+﻿#This script changes a UPN
+#For instance: from domain.local to domain.com, to be able to sync the domain with Office 365
+
+Import-Module ActiveDirectory
+
+#Variables
+$oldSuffix = "unimop.lan"
+$newSuffix = "unimop.com"
+$ou = "OU=Users,OU=Unimop,DC=unimop,DC=lan"
+
+#Get and set UPN's
+Get-ADUser -SearchBase $ou -Filter * | ForEach-Object {
+    $newUpn = $_.UserPrincipalName.Replace($oldSuffix,$newSuffix)
+    Set-ADUser $_ -UserPrincipalName $newUpn
+}
+
+exit
--- Install MSOnline and AzureAD modules.ps1
+++
@@ -1,5 +0,0 @@
-﻿Install-PackageProvider -Name NuGet -Force -Confirm:$false
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module AzureAD -Force -Confirm:$false
-Install-Module MSOnline -Force -Confirm:$false
-Exit
