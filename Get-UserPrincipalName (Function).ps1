90778fcf46acae54b739a1925b3e2d16b9b0c72e
wiseleaf23 <52524745+wiseleaf23@users.noreply.github.com>
2022-05-31 13:39:10 +0200

    Update Get-UserPrincipalName (Function).ps1

--- Get-UserPrincipalName (Function).ps1
+++ Get-UserPrincipalName (Function).ps1
@@ -14,25 +14,24 @@
         User Principal Name, also known as UPN
 
     .NOTES
-        Version:          1.3
+        Version:          1.4
         Template version: 1.4
         Author:           Axel Timmermans
         Creation Date:    2020-01-17
-        Last change:      Updated help text, prepared for publication
+        Last change:      Updated function - Updated help text, prepared for publication
 
     .EXAMPLE
         $UPN = Get-UserPrincipalName
     #>
     try{
-        $objUser = New-Object System.Security.Principal.NTAccount($Env:USERNAME)
-        $strSID = ($objUser.Translate([System.Security.Principal.SecurityIdentifier])).Value
-        $basePath = "HKLM:\SOFTWARE\Microsoft\IdentityStore\Cache\$strSID\IdentityCache\$strSID"
-        $UPN = (Get-ItemProperty -Path $basePath -Name UserName).UserName
+        $SID = ((New-Object System.Security.Principal.NTAccount($Env:USERNAME)).Translate([System.Security.Principal.SecurityIdentifier])).Value
+        $UPN = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\IdentityStore\Cache\$SID\IdentityCache\$SID" -Name UserName).UserName
         #Write error if UPN is Null
         If(!$UPN){
             Write-Error "Null UPN detected"
+        }else{
+            Return $UPN
         }
-        Return $UPN
     }catch{
         Write-Error "Failed to auto detect user principal name"
     }
